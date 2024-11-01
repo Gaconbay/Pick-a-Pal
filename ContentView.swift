464c94058a3c6aa2898f9ba0695babc14e72f9e1
Ty Tran <cmtran1400@gmail.com>
2024-11-01 14:57:16 -0500

    Added TextField

--- ContentView.swift
+++ ContentView.swift
@@ -10,13 +10,30 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var names : [String] = ["Elisha", "Andre", "Jasmine", "Po-Chun"]
+    @State private var nameToAdd = ""
+    @State private var pickedName = ""
     
     var body: some View {
         
         VStack {
+            
+            Text(pickedName.isEmpty ? "" : pickedName)
+            
             List{
-                
+                ForEach(names, id: \.description) { name in
+                    Text(name)
+                }
             }
+            
+            TextField("Add Name", text: $nameToAdd) // '$' symbol represents a binding to a variable
+                .autocorrectionDisabled()
+                .onSubmit{
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    
+                    }
+                }
         }
         .padding()
     }
