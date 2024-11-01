4cc8bdf9475e45c4e6374079097874ae83e9edb7
Ty Tran <cmtran1400@gmail.com>
2024-11-01 14:18:59 -0500

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Pick-a-Pal
+//
+//  Created by Ty Tran on 11/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var names : [String] = ["Elisha", "Andre", "Jasmine", "Po-Chun"]
+    
+    var body: some View {
+        
+        VStack {
+            List{
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
